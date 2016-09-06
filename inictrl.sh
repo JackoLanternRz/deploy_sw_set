89aabe40a806bc8bd9613439068c04ceb7693fa5
JackoLanternRz <protoss914@gmail.com>
2016-09-06 08:57:44 +0800

    add Readme and some aid scripts

---
+++ inictrl.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+ovs-vsctl set-controller s1 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s2 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s3 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s4 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s5 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s6 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s7 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s8 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s9 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s10 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s11 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s12 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s13 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s14 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s15 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s16 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s17 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s18 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s19 tcp:192.168.1.11:6633 
+ovs-vsctl set-controller s20 tcp:192.168.1.11:6633 
---
+++ rmctrl.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+ovs-vsctl set-controller s1001 
+ovs-vsctl set-controller s1002 
+ovs-vsctl set-controller s1003
+ovs-vsctl set-controller s1004 
+ovs-vsctl set-controller s2001 
+ovs-vsctl set-controller s2002 
+ovs-vsctl set-controller s2003 
+ovs-vsctl set-controller s2004 
+ovs-vsctl set-controller s2005 
+ovs-vsctl set-controller s2006 
+ovs-vsctl set-controller s2007 
+ovs-vsctl set-controller s2008 
+ovs-vsctl set-controller s3001 
+ovs-vsctl set-controller s3002 
+ovs-vsctl set-controller s3003 
+ovs-vsctl set-controller s3004 
+ovs-vsctl set-controller s3005 
+ovs-vsctl set-controller s3006 
+ovs-vsctl set-controller s3007 
+ovs-vsctl set-controller s3008
